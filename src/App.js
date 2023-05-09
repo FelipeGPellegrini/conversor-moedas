7f8a73afbcabb01cfc2e76cda67cb38bf7e96b26
Felipe Gomes Pellegrini <107892258+FelipeGPellegrini@users.noreply.github.com>
2023-05-09 18:01:56 -0300

    user

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 import Conversor from './componentes/Conversor';
+import { useState } from 'react';
 
 function App() {
   return (
     <>
-    <div className="App">
-      <h1>Conversor de Moedas</h1>
-      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
-      <Conversor moedaA="BRL" moedaB="USD"></Conversor>
-      <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
-      <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
-    </div>
-    <p>Feito com ❤️ por <a href="https://github.com/FelipeGPellegrini">Felipe Pellegrini</a></p>
+      <div className="App">
+        <h1>Conversor de Moedas</h1>
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+        <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+      <p>Feito com ❤️ por <a href="https://github.com/FelipeGPellegrini">Felipe Pellegrini</a></p>
     </>
   );
 }
